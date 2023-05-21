1e40ba2ed794550cb636653cf3c3312477156d3e
hcehajic <harun.cehajic00@gmail.com>
2023-05-21 15:22:42 +0200

    More minor changes

--- src/main/java/ba/unsa/etf/cehajic/hcehajic2/appback/SubTask/SubTaskConfig.java
+++ src/main/java/ba/unsa/etf/cehajic/hcehajic2/appback/SubTask/SubTaskConfig.java
@@ -15,9 +15,8 @@ public class SubTaskConfig {
         return args -> {
             SubTask s1 = new SubTask("SubDescription1", Long.valueOf(1), Long.valueOf(1));
             SubTask s2 = new SubTask("SubDescription2", Long.valueOf(1), Long.valueOf(1));
-            repository.saveAll(
-                    List.of(s1, s2)
-            );
+            repository.save(s1);
+            repository.save(s2);
         };
     }
 }
