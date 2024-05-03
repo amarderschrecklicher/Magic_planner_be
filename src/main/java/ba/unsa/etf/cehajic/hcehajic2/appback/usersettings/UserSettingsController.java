b70f6049d3f36e13afa42dffaa1206e70b986a9d
dalilakrslak <dkrslak1@etf.unsa.ba>
2024-05-03 15:39:57 +0200

    update user settings

--- src/main/java/ba/unsa/etf/cehajic/hcehajic2/appback/usersettings/UserSettingsController.java
+++ src/main/java/ba/unsa/etf/cehajic/hcehajic2/appback/usersettings/UserSettingsController.java
@@ -26,13 +26,6 @@ public class UserSettingsController {
     @GetMapping(path="/{id}")
     public UserSettings GetUserSettings(@PathVariable("id") Long id) { return userSettingsService.GetUserSettingsForAccount(id); }
 
-//    @PutMapping(path="/update")
-//    public void Update() {
-//
-//        userSettingsService.UpdateAccountId(Long.valueOf(1), Long.valueOf(1));
-//        userSettingsService.UpdateAccountId(Long.valueOf(2), Long.valueOf(2));
-//    }
-
     @GetMapping(path="mobile/{id}")
     public String GetPhoneCode(@PathVariable Long id) {
         return userSettingsService.GetPhoneCode(id);
@@ -49,148 +42,55 @@ public class UserSettingsController {
         UserSettings newUserSettings = userSettingsService.CreateUserSettingsDefault(id);
         return ResponseEntity.ok().body(newUserSettings);
     }
-    @PutMapping("/font/{id}")
-    public ResponseEntity<UserSettings> updateUserFont(@PathVariable Long id, @RequestBody String fontJson) {
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserSettings> updateUserSettings(@PathVariable Long id, @RequestBody String settingsJson) {
         try {
             ObjectMapper objectMapper = new ObjectMapper();
-            JsonNode jsonNode = objectMapper.readTree(fontJson);
-            String font = jsonNode.get("font").asText();
-            System.out.println(font);
-            UserSettings updatedUser = userSettingsService.UpdateFont(id, font);
-//            System.out.println(updatedUser);
-            if (updatedUser != null) {
-                return ResponseEntity.ok(updatedUser);
-            } else {
+            JsonNode jsonNode = objectMapper.readTree(settingsJson);
+            UserSettings existingSettings = userSettingsService.GetUserSettingsForAccount(id);
+
+            if (existingSettings == null) {
                 return ResponseEntity.notFound().build();
             }
+
+            if (jsonNode.has("font")) {
+                existingSettings.setFont(jsonNode.get("font").asText());
+            }
+
+            if (jsonNode.has("fontSize")) {
+                existingSettings.setFontSize(jsonNode.get("fontSize").asInt());
+            }
+
+            if (jsonNode.has("colorOfPriorityTask")) {
+                existingSettings.setColorOfPriorityTask(jsonNode.get("colorOfPriorityTask").asText());
+            }
+
+            if (jsonNode.has("colorOfNormalTask")) {
+                existingSettings.setColorOfNormalTask(jsonNode.get("colorOfNormalTask").asText());
+            }
+
+            if (jsonNode.has("colorForSubtask")) {
+                existingSettings.setColorForSubtask(jsonNode.get("colorForSubtask").asText());
+            }
+
+            if (jsonNode.has("colorForFont")) {
+                existingSettings.setColorForFont(jsonNode.get("colorForFont").asText());
+            }
+
+            if (jsonNode.has("colorForBackground")) {
+                existingSettings.setColorForBackground(jsonNode.get("colorForBackground").asText());
+            }
+
+            if (jsonNode.has("colorForProgress")) {
+                existingSettings.setColorForProgress(jsonNode.get("colorForProgress").asText());
+            }
+
+            userSettingsService.updateUserSettings(existingSettings);
+            return ResponseEntity.ok(existingSettings);
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
         }
     }
 
-    @PutMapping("/fontsize/{id}")
-    public ResponseEntity<UserSettings> updateUserFontSize(@PathVariable Long id,  @RequestBody String sizeJson) {
-        try {
-            ObjectMapper objectMapper = new ObjectMapper();
-            JsonNode jsonNode = objectMapper.readTree(sizeJson);
-            int size = jsonNode.get("size").asInt();
-
-            UserSettings updatedUser = userSettingsService.UpdateFontSize(id, size);
-//            System.out.println(updatedUser);
-            if (updatedUser != null) {
-                return ResponseEntity.ok(updatedUser);
-            } else {
-                return ResponseEntity.notFound().build();
-            }
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
-        }
-    }
-
-    @PutMapping("/priority/{id}")
-    public ResponseEntity<UserSettings> updateUserPriority(@PathVariable Long id, @RequestBody String priorityJson) {
-        try {
-            ObjectMapper objectMapper = new ObjectMapper();
-            JsonNode jsonNode = objectMapper.readTree(priorityJson);
-            String priority = jsonNode.get("priority").asText();
-
-            UserSettings updatedUser = userSettingsService.UpdatePriorityTask(id, priority);
-            if (updatedUser != null) {
-                return ResponseEntity.ok(updatedUser);
-            } else {
-                return ResponseEntity.notFound().build();
-            }
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
-        }
-    }
-
-    @PutMapping("/normal/{id}")
-    public ResponseEntity<UserSettings> updateUserNormal(@PathVariable Long id, @RequestBody String normalJson) {
-        try {
-            ObjectMapper objectMapper = new ObjectMapper();
-            JsonNode jsonNode = objectMapper.readTree(normalJson);
-            String normal = jsonNode.get("normal").asText();
-
-            UserSettings updatedUser = userSettingsService.UpdateNormalTask(id, normal);
-            if (updatedUser != null) {
-                return ResponseEntity.ok(updatedUser);
-            } else {
-                return ResponseEntity.notFound().build();
-            }
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
-        }
-    }
-
-    @PutMapping("/sub/{id}")
-    public ResponseEntity<UserSettings> updateUserSub(@PathVariable Long id, @RequestBody String subJson) {
-        try {
-            ObjectMapper objectMapper = new ObjectMapper();
-            JsonNode jsonNode = objectMapper.readTree(subJson);
-            String sub = jsonNode.get("sub").asText();
-
-            UserSettings updatedUser = userSettingsService.UpdateSubTask(id, sub);
-            if (updatedUser != null) {
-                return ResponseEntity.ok(updatedUser);
-            } else {
-                return ResponseEntity.notFound().build();
-            }
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
-        }
-    }
-
-    @PutMapping("/fcolor/{id}")
-    public ResponseEntity<UserSettings> updateUserHeader(@PathVariable Long id, @RequestBody String fontColorJson) {
-        try {
-            ObjectMapper objectMapper = new ObjectMapper();
-            JsonNode jsonNode = objectMapper.readTree(fontColorJson);
-            String fontColor = jsonNode.get("fcolor").asText();
-
-            UserSettings updatedUser = userSettingsService.UpdateFontColor(id, fontColor);
-            if (updatedUser != null) {
-                return ResponseEntity.ok(updatedUser);
-            } else {
-                return ResponseEntity.notFound().build();
-            }
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
-        }
-    }
-
-    @PutMapping("/background/{id}")
-    public ResponseEntity<UserSettings> updateUserBackground(@PathVariable Long id, @RequestBody String bgJson) {
-        try {
-            ObjectMapper objectMapper = new ObjectMapper();
-            JsonNode jsonNode = objectMapper.readTree(bgJson);
-            String background = jsonNode.get("background").asText();
-
-            UserSettings updatedUser = userSettingsService.UpdateBackground(id, background);
-            if (updatedUser != null) {
-                return ResponseEntity.ok(updatedUser);
-            } else {
-                return ResponseEntity.notFound().build();
-            }
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
-        }
-    }
-    @PutMapping("/progress/{id}")
-    public ResponseEntity<UserSettings> updateUserProgress(@PathVariable Long id, @RequestBody String progressJson) {
-        try {
-            ObjectMapper objectMapper = new ObjectMapper();
-            JsonNode jsonNode = objectMapper.readTree(progressJson);
-            String progress = jsonNode.get("progress").asText();
-
-            UserSettings updatedUser = userSettingsService.UpdateProgress(id, progress);
-            if (updatedUser != null) {
-                return ResponseEntity.ok(updatedUser);
-            } else {
-                return ResponseEntity.notFound().build();
-            }
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
-        }
-    }
 }
--- src/main/java/ba/unsa/etf/cehajic/hcehajic2/appback/usersettings/UserSettingsService.java
+++ src/main/java/ba/unsa/etf/cehajic/hcehajic2/appback/usersettings/UserSettingsService.java
@@ -50,78 +50,15 @@ public class UserSettingsService {
         return matching;
     }
 
-    public UserSettings UpdateFont(Long id, String font) {
-        System.out.println(font);
-        UserSettings existingSettings = GetUserSettingsForAccount(id);
-        if (existingSettings == null) return null;
-        existingSettings.setFont(font);
-        userSettingsRepository.save(existingSettings);
-//        System.out.printf(String.valueOf(GetUserSettingsForAccount(id)));
-        return existingSettings;
-    }
-
-    public UserSettings UpdateFontSize(Long id, int font) {
-        UserSettings existingSettings = GetUserSettingsForAccount(id);
-        if (existingSettings == null) return null;
-        existingSettings.setFontSize(font);
-        userSettingsRepository.save(existingSettings);
-//        UserSettings e = GetUserSettingsForAccount(id);
-//        System.out.printf(e.toString());
-        return existingSettings;
-    }
-
-    public UserSettings UpdatePriorityTask(Long id, String rgb) {
-        UserSettings existingSettings = GetUserSettingsForAccount(id);
-        if (existingSettings == null) return null;
-        existingSettings.setColorOfPriorityTask(rgb);
-        userSettingsRepository.save(existingSettings);
-        return existingSettings;
-    }
-
-    public UserSettings UpdateNormalTask(Long id, String rgb) {
-        UserSettings existingSettings = GetUserSettingsForAccount(id);
-        if (existingSettings == null) return null;
-        existingSettings.setColorOfNormalTask(rgb);
-        userSettingsRepository.save(existingSettings);
-        return existingSettings;
-    }
-
-    public UserSettings UpdateSubTask(Long id, String rgb) {
-        UserSettings existingSettings = GetUserSettingsForAccount(id);
-        if (existingSettings == null) return null;
-        existingSettings.setColorForSubtask(rgb);
-        userSettingsRepository.save(existingSettings);
-        return existingSettings;
-    }
-
-    public UserSettings UpdateFontColor(Long id, String rgb) {
-        UserSettings existingSettings = GetUserSettingsForAccount(id);
-        if (existingSettings == null) return null;
-        existingSettings.setColorForFont(rgb);
-        userSettingsRepository.save(existingSettings);
-        return existingSettings;
-    }
-
-    public UserSettings UpdateBackground(Long id, String rgb) {
-        UserSettings existingSettings = GetUserSettingsForAccount(id);
-        if (existingSettings == null) return null;
-        existingSettings.setColorForBackground(rgb);
-        userSettingsRepository.save(existingSettings);
-        return existingSettings;
-    }
-
-    public UserSettings UpdateProgress(Long id, String rgb) {
-        UserSettings existingSettings = GetUserSettingsForAccount(id);
-        if (existingSettings == null) return null;
-        existingSettings.setColorForProgress(rgb);
-        userSettingsRepository.save(existingSettings);
-        return existingSettings;
-    }
-
     public String GetPhoneCode(Long id) {
         UserSettings userSettings = GetUserSettingsForAccount(id);
         if (userSettings == null) return null;
         return userSettings.getPhoneLoginString();
+    }
+
+    public UserSettings updateUserSettings(UserSettings userSettings) {
+        userSettingsRepository.save(userSettings);
+        return userSettings;
     }
 
     public void UpdateAccountId(Long aid, Long sid) {
