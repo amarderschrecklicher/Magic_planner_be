2ca90fbef30a2b805c19aa699612a4954a674acd
amarderschrecklicher <115952128+amarderschrecklicher@users.noreply.github.com>
2024-07-01 11:53:39 +0200

    deployment

--- src/main/java/ba/unsa/etf/cehajic/hcehajic2/appback/task/TaskSchedulerService.java
+++ src/main/java/ba/unsa/etf/cehajic/hcehajic2/appback/task/TaskSchedulerService.java
@@ -7,6 +7,8 @@ import ba.unsa.etf.cehajic.hcehajic2.appback.token.Token;
 import ba.unsa.etf.cehajic.hcehajic2.appback.token.TokenService;
 
 import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 
@@ -24,16 +26,19 @@ public class TaskSchedulerService {
     private TokenService tokenService;
 
     public void checkTasksEndingSoon() {
-
-        LocalDateTime a = LocalDateTime.now().plusMinutes(29).plusSeconds(30);
-        LocalDateTime b = LocalDateTime.now().plusMinutes(30).plusSeconds(30);
-
+        // Convert local times to ZonedDateTime in UTC
+        ZonedDateTime a = LocalDateTime.now().plusMinutes(29).plusSeconds(30).atZone(ZoneId.systemDefault()).withZoneSameInstant(ZoneId.of("UTC"));
+        ZonedDateTime b = LocalDateTime.now().plusMinutes(30).plusSeconds(30).atZone(ZoneId.systemDefault()).withZoneSameInstant(ZoneId.of("UTC"));
+    
+        // Format ZonedDateTime to ISO string
         String A = a.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
         String B = b.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
-
+    
+        // Call the repository method with the formatted time strings
         List<Task> tasksEndingSoon = taskRepository.findTasksEndingInNext30Minutes(A, B);
         System.out.println(tasksEndingSoon);
-
+    
+        // Iterate over the tasks and send notifications
         for (Task task : tasksEndingSoon) {
             List<Token> pushTokens = tokenService.GetTokensForAccount(task.getChild().getId());
             System.out.println(pushTokens);
@@ -41,7 +46,6 @@ public class TaskSchedulerService {
             for (Token pushToken : pushTokens) {
                 notificationService.sendMobileNotification(pushToken, task, "Još 30min");
             }
-
         }
     }
 }
